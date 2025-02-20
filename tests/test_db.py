85d7b502793c4289e3d41e86f9a81efa517e4f7d
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-02-20 11:24:52 +0100

    test: Test structure flag

--- tests/test_db.py
+++ tests/test_db.py
@@ -11,3 +11,7 @@ def test_Ce2Fe14B():
 def test_NdFe14B():
     with pytest.raises(LookupError):
         Ms_0, A_0, K_0 = mammos_dft.db.get_material_parameters("NdFe14B")
+
+def test_NdFe14B_A1():
+    Ms_0, A_0, K_0 = mammos_dft.db.get_material_parameters("NdFe14B", structure="A1")
+    assert (Ms_0, A_0, K_0) == (0.99632415793215,4.8615622027493e-12,1189274.36002828)
