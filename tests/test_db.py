d3d51d5b572ceb7fe5c1b2a122eb1677bf2c1531
Andrea Petrocchi <andrea.petrocchi@mpsd.mpg.de>
2025-02-20 11:24:35 +0100

    test: Use `pytest.raises`

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,5 +1,7 @@
 """Test db lookup."""
 
+import pytest
+
 import mammos_dft
 
 def test_Ce2Fe14B():
@@ -7,8 +9,5 @@ def test_Ce2Fe14B():
     assert (Ms_0, A_0, K_0) == (0.99632415793215,4.8615622027493e-12,1189274.36002828)
 
 def test_NdFe14B():
-    try:
+    with pytest.raises(LookupError):
         Ms_0, A_0, K_0 = mammos_dft.db.get_material_parameters("NdFe14B")
-        assert False
-    except LookupError:
-        assert True
